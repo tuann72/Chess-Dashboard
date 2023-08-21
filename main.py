d27162ba845a1f7df96b2e5cd29e32267f469fd2
Tuan Nguyen <tuannguyen08426@gmail.com>
2023-08-21 12:44:14 -0500

    Added symbol. (Unsure if the symbol is working)

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ root = tk.Tk()
 root.geometry("800x600")
 root.title("Chess-Statistics-Made-Simple")
 
+"""
+Check if code works on windows.
+"""
+
+root.iconbitmap("resources/chess.ico")
+
 # App Run
 root.mainloop()
